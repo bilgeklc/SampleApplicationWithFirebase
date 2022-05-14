0a3ea63aac9bd2a4373b6bb41a5dbb73c624881b
kaya <bilgehan_446@hotmail.com>
2022-05-15 00:20:54 +0300

    Feed actions

--- app/src/main/java/com/bilge/kotlinfirebase/FeedActivity.kt
+++ app/src/main/java/com/bilge/kotlinfirebase/FeedActivity.kt
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bilge.kotlinfirebase.adapter.FeedRecyclerAdapter
 import com.bilge.kotlinfirebase.databinding.ActivityFeedBinding
+import com.bilge.kotlinfirebase.model.Post
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityFeedBinding
     private lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
+    private lateinit var postArrayList : ArrayList<Post>
+    private lateinit var feedAdapter : FeedRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +32,51 @@ class FeedActivity : AppCompatActivity() {
         setContentView(view)
 
         auth = Firebase.auth
+        db = Firebase.firestore
+
+        postArrayList = ArrayList<Post>()
+
+        getData()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        feedAdapter = FeedRecyclerAdapter(postArrayList)
+        binding.recyclerView.adapter = feedAdapter
+
+
+    }
+
+    private fun getData() {
+
+        db.collection("Posts").orderBy("date", Query.Direction.DESCENDING).addSnapshotListener { value, error ->
+
+            if(error != null){
+                Toast.makeText(this, error.localizedMessage, Toast.LENGTH_LONG).show()
+            }else{
+                if(value != null){
+                    if(!value.isEmpty){
+
+                        val documents = value.documents
+
+                        postArrayList.clear()
+
+                        for (document in documents) {
+
+                            val comment = document.get("comment") as String
+                            val userEmail = document.get ("userEmail") as String
+                            val downloadUrl = document.get("downloadUrl") as String
+
+                            val post = Post(userEmail,comment,downloadUrl)
+                            postArrayList.add(post)
+
+                        }
+
+                        feedAdapter.notifyDataSetChanged()
+
+                    }
+                }
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
---
+++ app/src/main/java/com/bilge/kotlinfirebase/adapter/FeedRecyclerAdapter.kt
@@ -0,0 +1,30 @@
+package com.bilge.kotlinfirebase.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bilge.kotlinfirebase.databinding.RecyclerRowBinding
+import com.bilge.kotlinfirebase.model.Post
+import com.squareup.picasso.Picasso
+
+class FeedRecyclerAdapter(private val postList : ArrayList<Post>) : RecyclerView.Adapter<FeedRecyclerAdapter.PostHolder>() {
+
+    class PostHolder(val binding : RecyclerRowBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return PostHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+        holder.binding.recyclerEmailText.text = postList.get(position).email
+        holder.binding.recyclerCommentText.text = postList.get(position).comment
+        Picasso.get().load(postList.get(position).downloadUrl).into(holder.binding.recyclerImageView)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
